802161ef03b6ba5757b0a7aa3dfb179731a9dee2
Anobhama <anobhama99.com>
2021-07-19 11:52:28 +0530

    basics

---
+++ TypeScript/DataType.ts
@@ -0,0 +1,13 @@
+var Name1:String="Anu";
+console.log(Name1);
+
+var num:Number=23;
+console.log(num);
+
+//seeing the type
+console.log(typeof(num));
+
+
+//single line declarations
+var sal:Number,name2:String="Anu";
+console.log(sal);
---
+++ TypeScript/Datatype.js
@@ -0,0 +1,9 @@
+var Name1 = "Anu";
+console.log(Name1);
+var num = 23;
+console.log(num);
+//seeing the type
+console.log(typeof (num));
+//single line declarations
+var sal, name2 = "Anu";
+console.log(sal);
