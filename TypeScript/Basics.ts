3dd3817cbff4e15e28c9bff60245a4ce44e871e5
Anobhama <anobhama99.com>
2021-07-19 12:11:33 +0530

    if else and switch case

---
+++ TypeScript/Basics.ts
@@ -0,0 +1,22 @@
+let num1:number=10, num2=20;
+if( num1<num2)
+{
+    console.log(" num1 is smaller");
+}
+else if(num1<num2){
+    console.log("num2 is greater ");
+}
+else{
+    console.log("error ");
+}
+
+let day:number=3;
+switch(day)
+{
+case 0: console.log("Sunday");
+    break;
+case 1: console.log("Monday");
+    break; 
+default: console.log("there r only 7 days ");
+break;
+}
