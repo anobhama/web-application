dd07496b35af1cf37954e09ab3c55f1e68b06d2b
Anobhama <anobhama99.com>
2021-07-19 11:58:40 +0530

    arrays with for + while

---
+++ TypeScript/Arrays.js
@@ -0,0 +1,10 @@
+var arr1 = [1, 2, 3, 4];
+for (var i in arr1) {
+    console.log(i);
+    //console.log(arr1[i]);
+}
+var k = 1;
+while (k < 5) {
+    console.log(k);
+    k++;
+}
---
+++ TypeScript/Arrays.ts
@@ -0,0 +1,16 @@
+let arr1:Array<Number>=[1,2,3,4];
+
+for(let i in arr1)
+{
+    console.log(i);
+    //console.log(arr1[i]);
+}
+
+
+let k:number=1;
+while(k<5)
+{
+    console.log(k);
+    k++;
+}
+
