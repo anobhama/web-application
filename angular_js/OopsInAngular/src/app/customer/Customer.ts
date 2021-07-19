5bbfc0901dc8cf0d7df9933441e46b0c64bf0b54
Anobhama <anobhama99.com>
2021-07-19 14:56:23 +0530

    Displaying customer details

---
+++ angular_js/OopsInAngular/src/app/customer/Customer.ts
@@ -0,0 +1,8 @@
+export class Customer
+{
+    custNo:number=0;
+    custName:string="";
+    custAdd:string="";
+    city:string="";
+    country:string="";
+}
--- angular_js/OopsInAngular/src/app/customer/customer.component.ts
+++ angular_js/OopsInAngular/src/app/customer/customer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from './Customer';
 
 @Component({
   selector: 'app-customer',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+
+  custList:Customer[] = [{custNo:1,custName:"Anu",custAdd:"PN Colony",city:"Nagercoil",country:"India"},
+  {custNo:2,custName:"Tilak",custAdd:"Porur",city:"Chni",country:"India"},
+  {custNo:3,custName:"Asu",custAdd:"Vadasery",city:"Nagercoil",country:"India"}];
+
 
   constructor() { }
 
