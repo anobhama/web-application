0da4449c0f411759f1c73295af8b642dd00007b2
Anobhama <anobhama99.com>
2021-07-20 14:20:06 +0530

    added method in services

--- angular_js/Routing/src/app/sample.service.ts
+++ angular_js/Routing/src/app/sample.service.ts
@@ -5,6 +5,20 @@ import { Injectable } from '@angular/core';
 })
 export class SampleService {
 
-  
+  //add( 10,20)  //add( 10,20,30,40) 
+  // rest parameters 
+  public add(...params: number[]): number {
+    let result = 0;
+    for (let val of params) {
+        result += val;
+    }
+    return result;
+  } 
+ //multi(10,20)  
+  public multi(x:number, y :number): number {
+    let result = 0;    
+    result=x*y;
+    return result;
+  }
   constructor() { }
 }
