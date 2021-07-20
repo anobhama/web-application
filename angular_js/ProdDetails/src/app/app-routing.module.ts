d92d618dd023cd2a8614f73608da78ff28dbf393
Anobhama <anobhama99.com>
2021-07-20 12:36:52 +0530

    routing done

--- angular_js/ProdDetails/src/app/app-routing.module.ts
+++ angular_js/ProdDetails/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { SalesComponent } from './sales/sales.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"Products", component:ProductsComponent},
+  {path:"Sales", component:SalesComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
