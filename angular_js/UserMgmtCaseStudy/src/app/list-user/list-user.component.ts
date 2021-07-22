f28a7f1998fd92eb82f9e6c2f9d60b2bf867541c
Anobhama <anobhama99.com>
2021-07-22 10:08:18 +0530

    list user display done

--- angular_js/UserMgmtCaseStudy/src/app/list-user/list-user.component.ts
+++ angular_js/UserMgmtCaseStudy/src/app/list-user/list-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../User';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-list-user',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-user.component.css']
 })
 export class ListUserComponent implements OnInit {
+  users: User[]=[];							
+							
+    constructor(private router: Router, private userService:UserService) { }							
+                
+    // Initialize with default list of users							
+    ngOnInit() {							
+      if(localStorage.getItem("username")!=null){							
+       							
+      this.userService.getUsers()							
+        .subscribe((data: User[])=> {							
+          this.users = data;							
+        });							
+      }							
+      else							
+      this.router.navigate(['/login']);							
+      							
+    }							
+                
+    // logOff user							
+    logOutUser():void{							
+        if(localStorage.getItem("username")!=null){							
+          localStorage.removeItem("username");							
+          this.router.navigate(['/login']);							
+        }							
+    }							
+  }							
+  /*
+  // Modify USer							
+  editUser(user: User): void {							
+    localStorage.removeItem("editUserId");							
+    localStorage.setItem("editUserId", user.id.toString());							
+    this.router.navigate(['edit-user']);							
+  };							
+                
+  // Add New User							
+  addUser(): void {							
+    this.router.navigate(['add-user']);							
+  };
+*/
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
